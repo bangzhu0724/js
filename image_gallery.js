36a0570a5e7572a7012c4ad41c5ee2d1d98ef4e5
bangzhu0724 <983958025@qq.com>
2013-11-06 04:13:58 +0800

    Update image_gallery.js

--- image_gallery.js
+++ image_gallery.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+	$(".btn").show();
+	$(".btn  a:first").addClass("active");	
+	$().gallery({
+		current: [".show_images_1",".show_images_1_img"],
+		left: [".show_images_2",".show_images_2_img"],
+		right: [".show_images_3",".show_images_3_img"],
+	none: [".show_images_4",".show_images_4_img"],
+		duration: 500,
+		start: function() {
+			$(".header_text").fadeOut(150);
+		},
+		end: function() {
+			$(".header_text").fadeIn(150);
+		},
+		autoChange : true,
+		changeTimeout: 3000,
+		stopTarget : ".header_stage"
+	});
+});
 var time=new Date();
 time=time.getTime();
 var userAgent = navigator.userAgent.toLowerCase(); 
